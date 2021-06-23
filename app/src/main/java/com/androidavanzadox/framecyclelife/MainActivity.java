d5c931c0d9ba498aeabda953c1728085d64bb6f7
Edson Valenzuela <evalenzuela@tecniseguros.com>
2021-06-23 12:52:34 -0600

    Initial commit

---
+++ app/src/main/java/com/androidavanzadox/framecyclelife/MainActivity.java
@@ -0,0 +1,20 @@
+package com.androidavanzadox.framecyclelife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
